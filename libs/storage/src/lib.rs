d363bb7408978cb5a8ce5a207d89ca2e5ea18e22
spectrecoder <pong96111kind@gmail.com>
2024-08-30 14:19:30 -0700

    update storage

--- libs/storage/src/lib.rs
+++ libs/storage/src/lib.rs
@@ -29,3 +29,24 @@ impl Instance {
         env.storage().instance().remove(key);
     }
 }
+
+pub struct Persistent;
+impl Persistent {
+    pub fn get<K: IntoVal<Env, Val>, V: TryFromVal<Env, Val>>(env: &Env, key: &K) -> Option<V> {
+        env.storage().persistent().get::<K, V>(key)
+    }
+    pub fn set<K: IntoVal<Env, Val>, V: IntoVal<Env, Val>>(env: &Env, key: &K, val: &V) {
+        env.storage().persistent().set(key, val);
+    }
+    pub fn has<K: IntoVal<Env, Val>>(env: &Env, key: &K) -> bool {
+        env.storage().persistent().has(key)
+    }
+    pub fn extend<K: IntoVal<Env, Val>>(env: &Env, key: &K, min_ledger_to_live: u32) {
+        env.storage()
+            .persistent()
+            .extend_ttl(key, min_ledger_to_live, min_ledger_to_live)
+    }
+    pub fn remove<K: IntoVal<Env, Val>>(env: &Env, key: &K) {
+        env.storage().persistent().remove(key);
+    }
+}
