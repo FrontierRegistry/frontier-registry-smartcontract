78efcf0d29ec954d733c339e8e229dce018e93a8
spectrecoder <pong96111kind@gmail.com>
2024-09-04 07:23:15 -0700

    add types in frontier registry

--- contracts/Frontier-registry-contract/src/lib.rs
+++ contracts/Frontier-registry-contract/src/lib.rs
@@ -1,12 +1,21 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, Address, symbol_short, vec, Env, Symbol, Vec};
+use soroban_sdk::{contract, contractimpl, contractimport, Address, String, symbol_short, vec, Env, Symbol, Vec};
+
+mod types;
+
+pub mod frontier_nft {
+    contractimport!(
+        file = "../../target/wasm32-unknown-unknown/release/frontier_nft.wasm"
+    );
+    pub type FrontierNftClient<'a> = Client<'a>;
+}
 
 #[contract]
 pub struct FrontierRegistryContract;
 
 #[contractimpl]
 impl FrontierRegistryContract {
-    pub fn initialize(env: Env, admin: Address) {
+    pub fn initialize(env: &Env, frontier_nft_address: Address) {
 
     }
 }
