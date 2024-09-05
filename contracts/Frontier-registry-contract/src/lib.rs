33e6570bea721cbf67183b8233204a987c7add30
spectrecoder <pong96111kind@gmail.com>
2024-09-04 21:51:17 -0700

    add register_research

--- contracts/Frontier-registry-contract/src/lib.rs
+++ contracts/Frontier-registry-contract/src/lib.rs
@@ -9,7 +9,6 @@ pub mod frontier_nft {
     soroban_sdk::contractimport!(
         file = "../../target/wasm32-unknown-unknown/release/frontier_nft.wasm"
     );
-    pub type FrontierNftClient<'a> = Client<'a>;
 }
 
 use types::*;
@@ -22,7 +21,18 @@ pub struct FrontierRegistryContract;
 impl FrontierRegistryContract {
     pub fn initialize(env: &Env, frontier_nft_address: Address) {
     }
-    pub fn register_research(env: &Env, title: String, description: String, uri: String) {
+    pub fn register_research(env: &Env, contract: Address, to: Address, title: String, description: String, uri: String) -> u32 {
+        let frontier_nft_client = frontier_nft::Client::new(&env, &contract);
+
+        let token_id = frontier_nft_client.mint(&to, &title, &description, &uri);
+
+        token_id
+    }
+    pub fn get_nfts_by_user(env: &Env, user_address: Address) {
+
+    }
+    pub fn get_nft_info(env: &Env, nft_id: u32) {
+
     }
 }
 
